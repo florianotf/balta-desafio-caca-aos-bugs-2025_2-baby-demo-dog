58866b898ad83f24fb41b4c1121f9a2ab30fca25
Thiago Floriano <floriano.tfu@gmail.com>
2025-10-29 13:20:37 -0300

    Configuração DB

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,33 @@
+using BugStore.Data;
+using BugStore.Handlers.CreateCustomerHandler;
+using BugStore.Handlers.Customers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddTransient<ICreateCustomerHandler, CreateCustomerHandler>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+// app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/v1/customers", () => "Hello World!");
-app.MapGet("/v1/customers/{id}", () => "Hello World!");
-app.MapPost("/v1/customers", () => "Hello World!");
-app.MapPut("/v1/customers/{id}", () => "Hello World!");
-app.MapDelete("/v1/customers/{id}", () => "Hello World!");
+// app.MapGet("/v1/customers", () => "Hello World!");
+// app.MapGet("/v1/customers/{id}", () => "Hello World!");
+// app.MapPost("/v1/customers", () => );
+// app.MapPut("/v1/customers/{id}", () => "Hello World!");
+// app.MapDelete("/v1/customers/{id}", () => "Hello World!");
 
-app.MapGet("/v1/products", () => "Hello World!");
-app.MapGet("/v1/products/{id}", () => "Hello World!");
-app.MapPost("/v1/products", () => "Hello World!");
-app.MapPut("/v1/products/{id}", () => "Hello World!");
-app.MapDelete("/v1/products/{id}", () => "Hello World!");
+// app.MapGet("/v1/products", () => "Hello World!");
+// app.MapGet("/v1/products/{id}", () => "Hello World!");
+// app.MapPost("/v1/products", () => "Hello World!");
+// app.MapPut("/v1/products/{id}", () => "Hello World!");
+// app.MapDelete("/v1/products/{id}", () => "Hello World!");
 
-app.MapGet("/v1/orders/{id}", () => "Hello World!");
-app.MapPost("/v1/orders", () => "Hello World!");
+// app.MapGet("/v1/orders/{id}", () => "Hello World!");
+// app.MapPost("/v1/orders", () => "Hello World!");
 
 app.Run();
